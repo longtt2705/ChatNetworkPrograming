ea9e6959a267dc83c9b554d6315db85a6608c38e
dotaallstars200 <dragonallstar270520@gmail.com>
2020-07-03 09:55:36 +0700

    Init

---
+++ src/Client/Client.java
@@ -0,0 +1,11 @@
+package Client;
+
+/**
+ * Client
+ *
+ * @Created by Long - StudentID : 18120455
+ * @Date 03/07/2020 - 9:55 AM
+ * @Description
+ **/
+public class Client {
+}
---
+++ src/Server/Server.java
@@ -0,0 +1,11 @@
+package Server;
+
+/**
+ * Server
+ *
+ * @Created by Long - StudentID : 18120455
+ * @Date 03/07/2020 - 9:55 AM
+ * @Description
+ **/
+public class Server {
+}
